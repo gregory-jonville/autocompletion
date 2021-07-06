640976b0a51360b61b8a8b63c3f90c27ad6e3546
Gregory <gregory.jonville@laplateforme.io>
2021-07-06 13:57:34 +0200

    connexion db

--- classes/database.php
+++ classes/database.php
@@ -1,41 +1,66 @@
 <?php
 class Database
 {
-   private $db_host;
-   private $db_login;
-   private $db_password;
-   private $db_name;
-   public $pdo;
+  // ATTRIBUTS
+  public $pdo;
 
-   public function __construct()
-   {
-       $this->db_host = "localhost";
-       $this->db_login = "root";
-       $this->db_password = "root";
-       $this->db_name = "autocompletion";
-   }
+  // METHODES
+  // Constructeur
+  public function __construct()
+  {
+    $this->host = 'localhost';
+    $this->username = 'root';
+    $this->password = 'root';
+    $this->database = 'autocompletion';
+  }
 
-   public function connectDb()
-   {
-       try {
-       		// Connexion base de données
-           $this->pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name;charset=utf8", $this->db_login, $this->db_password);
-           // Paramètres requêtes
-           $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-           // Paramètres erreurs 
-           $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-               
-           return $this->pdo;
+  // Connexion database
+  public function connectDb()
+  {
 
-       } catch (PDOException $e) {
-           echo "Erreur de connexion à la base de données : " . $e->getMessage();
-        }
+    try { // Indique la requête SQL à lancer quand on se connecte
+      $this->pdo = new PDO(
+        'mysql:host=' . $this->host .
+          ';dbname=' . $this->database,
+        $this->username,
+        $this->password,
+
+        array(
+          // On définit l'attribut par défaut
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+          // On interragit avec la Base de données en UTF8 pour empêcher les problèmes d'accent
+          PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
+          // Mode d'erreur
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        )
+      );
+      // Retour de la connexion
+      return $this->pdo;
+      // Récupération des erreurs
+    } catch (PDOException $e) {
+      echo "Erreur de connexion à la base de données : " . $e->getMessage() . "<br/>";
+      die();
     }
+  }
 
-  public function close(){
+
+
+
+  public function close()
+  {
     $this->pdo = NULL;
   }
 
+  public function checkInput($var)
+    {
+        // Convertit les caractères spéciaux en entités HTML
+    	$var = htmlspecialchars($var);
+        // Supprime les espaces (ou d'autres caractères) en début et fin de chaîne
+    	$var = trim($var);
+      
+    	return $var;
+    }
 }
 
 $db = new Database();
+$con = $db->connectDb();
